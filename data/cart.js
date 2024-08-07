43d35bb6dc8624a9d4ddc5e12b7ba28c55097879
Dhruv Sabharwal <dhruvsabharwalh7@gmail.com>
2024-08-07 14:24:44 +0530

    complete ordersummary

--- data/cart.js
+++ data/cart.js
@@ -1,20 +1,11 @@
-export let cart = JSON.parse(localStorage.getItem('cart'));
-
-if(!cart){
-  cart = [{
-    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity:2
-  },{
-    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1
-  }];
-}
-[{
+export let cart = JSON.parse(localStorage.getItem('cart'))||[{
   productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-  quantity:2
+  quantity: 2,
+  deliveryOptionId: '1'
 },{
   productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-  quantity: 1
+  quantity: 1,
+  deliveryOptionId: '2'
 }];
 
 function saveToStorage(){
@@ -33,7 +24,8 @@ export function addToCart(productId,quantity){
   }else{
     cart.push({
       productId,
-      quantity
+      quantity,
+      deliveryOptionId: '1'
     });
   }
 
@@ -75,5 +67,18 @@ export function updateQuantity(productId, newQuantity) {
 
   matchingItem.quantity = newQuantity;
 
+  saveToStorage();
+}
+export function updateDeliveryOption(productId, deliveryOptionId){
+  let matchingProduct;
+
+  cart.forEach((cartItem) => {
+    if(productId === cartItem.productId){
+      matchingProduct = cartItem;
+    }
+  });
+
+  matchingProduct.deliveryOptionId = deliveryOptionId;
+  
   saveToStorage();
 }
