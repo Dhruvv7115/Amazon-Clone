2cb447388b4fbdabb22ae4e5f48248a42986d3d9
Dhruv Sabharwal <dhruvsabharwalh7@gmail.com>
2024-08-21 23:14:33 +0530

    Loading products from backend using callbacks

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,108 +1,112 @@
 import { cart } from '../data/cart-class.js';
-import {products} from '../data/products.js';
+import { products, loadProducts } from '../data/products.js';
 
-let productsHTML = '';
-products.forEach((product) => {
-  productsHTML += `
-    <div class="product-container">
-      <div class="product-image-container">
-        <img class="product-image"
-          src="${product.image}">
-      </div>
+loadProducts(renderProductsGrid);
 
-      <div class="product-name limit-text-to-2-lines">
-        ${product.name}
-      </div>
-
-      <div class="product-rating-container">
-        <img class="product-rating-stars"
-          src="${product.getStarsUrl()}">
-        <div class="product-rating-count link-primary">
-          ${product.rating.count}
+function renderProductsGrid(){
+  let productsHTML = '';
+  products.forEach((product) => {
+    productsHTML += `
+      <div class="product-container">
+        <div class="product-image-container">
+          <img class="product-image"
+            src="${product.image}">
         </div>
-      </div>
 
-      <div class="product-price">
-        ${product.getPrice()}
-      </div>
+        <div class="product-name limit-text-to-2-lines">
+          ${product.name}
+        </div>
 
-      <div class="product-quantity-container">
-        <select class="js-quantity-selector-${product.id}">
-          <option selected value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-          <option value="6">6</option>
-          <option value="7">7</option>
-          <option value="8">8</option>
-          <option value="9">9</option>
-          <option value="10">10</option>
-        </select>
-      </div>
-      <!-- 
-        we can use ternary operator to check if the product is instanceof Clothing if yes then we can return the link to size chart else return an empty string 
-      -->
-      <!-- 
-        1)But we have used 'Polymorphism'. 
-        2)Polymorphism : use a method w/o knowing the class.
-        3)extraInfoHTML() is defined in both the parent and child class but it returns the link to size chart in child class (Clothing) and returns an empty string in parent class (Product). 
-      -->
-      ${product.extraInfoHTML()}
+        <div class="product-rating-container">
+          <img class="product-rating-stars"
+            src="${product.getStarsUrl()}">
+          <div class="product-rating-count link-primary">
+            ${product.rating.count}
+          </div>
+        </div>
 
-      <div class="product-spacer"></div>
+        <div class="product-price">
+          ${product.getPrice()}
+        </div>
 
-      <div class="added-to-cart js-added-to-cart-${product.id}">
-        <img src="images/icons/checkmark.png">
-        Added
-      </div>
+        <div class="product-quantity-container">
+          <select class="js-quantity-selector-${product.id}">
+            <option selected value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+          </select>
+        </div>
+        <!-- 
+          we can use ternary operator to check if the product is instanceof Clothing if yes then we can return the link to size chart else return an empty string 
+        -->
+        <!-- 
+          1)But we have used 'Polymorphism'. 
+          2)Polymorphism : use a method w/o knowing the class.
+          3)extraInfoHTML() is defined in both the parent and child class but it returns the link to size chart in child class (Clothing) and returns an empty string in parent class (Product). 
+        -->
+        ${product.extraInfoHTML()}
 
-      <button class="add-to-cart-button button-primary js-add-to-cart-button"
-      data-product-id = "${product.id}">
-        Add to Cart
-      </button>
-    </div>`;
-});
-document.querySelector('.js-products-grid')
-  .innerHTML = productsHTML ;
+        <div class="product-spacer"></div>
 
-function updateCartQuantity(){
-  const cartQuantity = cart.calculateCartQuantity();
-  document.querySelector('.js-cart-quantity')
-    .innerHTML = cartQuantity;
-}
-updateCartQuantity();
+        <div class="added-to-cart js-added-to-cart-${product.id}">
+          <img src="images/icons/checkmark.png">
+          Added
+        </div>
 
-const addedMessageTimeouts = {};
+        <button class="add-to-cart-button button-primary js-add-to-cart-button"
+        data-product-id = "${product.id}">
+          Add to Cart
+        </button>
+      </div>`;
+  });
+  document.querySelector('.js-products-grid')
+    .innerHTML = productsHTML ;
 
-document.querySelectorAll('.js-add-to-cart-button')
-  .forEach((button) =>{
-    button.addEventListener('click',() => {
+  function updateCartQuantity(){
+    const cartQuantity = cart.calculateCartQuantity();
+    document.querySelector('.js-cart-quantity')
+      .innerHTML = cartQuantity;
+  }
+  updateCartQuantity();
 
-      const {productId} = button.dataset;
+  const addedMessageTimeouts = {};
 
-      const selectorValue = document.querySelector(`.js-quantity-selector-${productId}`);
-      
-      const quantity = Number(selectorValue.value);
+  document.querySelectorAll('.js-add-to-cart-button')
+    .forEach((button) =>{
+      button.addEventListener('click',() => {
 
-      cart.addToCart(productId,quantity);
+        const {productId} = button.dataset;
 
-      updateCartQuantity();
-      
-      const addedMessage = document.querySelector(`.js-added-to-cart-${productId}`);
-
-      const previousTimeoutId = addedMessageTimeouts[productId];
-      if (previousTimeoutId) {
-        clearTimeout(previousTimeoutId);
-      }
+        const selectorValue = document.querySelector(`.js-quantity-selector-${productId}`);
         
-      const timeoutId = setTimeout(() => {
-        addedMessage.classList.remove('added-to-cart-visible');
-      }, 2000);
+        const quantity = Number(selectorValue.value);
 
-      addedMessage.classList.add('added-to-cart-visible');
+        cart.addToCart(productId,quantity);
 
-      addedMessageTimeouts[productId] = timeoutId;
-      
-    })
-  })
+        updateCartQuantity();
+        
+        const addedMessage = document.querySelector(`.js-added-to-cart-${productId}`);
+
+        const previousTimeoutId = addedMessageTimeouts[productId];
+        if (previousTimeoutId) {
+          clearTimeout(previousTimeoutId);
+        }
+          
+        const timeoutId = setTimeout(() => {
+          addedMessage.classList.remove('added-to-cart-visible');
+        }, 2000);
+
+        addedMessage.classList.add('added-to-cart-visible');
+
+        addedMessageTimeouts[productId] = timeoutId;
+        
+      });
+    });
+}
