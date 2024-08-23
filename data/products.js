459737137a7f93d34f1052209ff1396aef97e517
Dhruv Sabharwal <dhruvsabharwalh7@gmail.com>
2024-08-23 14:39:10 +0530

    Learn error handling

--- data/products.js
+++ data/products.js
@@ -123,6 +123,8 @@ export function loadProductsFetch(){
       return new Product(productDetails);
     });
     console.log('load products');
+  }).catch((error) => {
+    console.log('Unexpected error. Please try again later.');
   });
   return promise;
 }
@@ -145,6 +147,10 @@ export function loadProducts(fun){
       return new Product(productDetails);
     });
     fun();
+  });
+
+  xhr.addEventListener('error', (error) => {
+    console.log('Unexpected error. Please try again later.');
   });
   xhr.open('GET', 'https://supersimplebackend.dev/products');
   xhr.send();
