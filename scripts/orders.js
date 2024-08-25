3c7d4e167806c57d08b014928327d03142d1b6dc
Dhruv Sabharwal <dhruvsabharwalh7@gmail.com>
2024-08-25 16:39:06 +0530

    Making the buy again and track package button interactive

--- scripts/orders.js
+++ scripts/orders.js
@@ -2,6 +2,7 @@ import { orders } from "../data/orders.js";
 import dayJs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import { formatCurrency } from "./utils/money.js";
 import { getProduct, loadProductsFetch } from "../data/products.js";
+import { cart } from "../data/cart-class.js";
 
 loadProductsFetch().then(() => {
   loadPage();
@@ -32,14 +33,15 @@ function loadPage(){
             <div class="product-quantity">
               Quantity: ${quantity}
             </div>
-            <button class="buy-again-button button-primary">
+            <button class="buy-again-button button-primary js-buy-again-button"
+            data-product-id="${productId}">
               <img class="buy-again-icon" src="images/icons/buy-again.png">
               <span class="buy-again-message">Buy it again</span>
             </button>
           </div>
   
           <div class="product-actions">
-            <a href="tracking.html">
+            <a href="tracking.html?orderId=${order.id}&productId=${productId}">
               <button class="track-package-button button-secondary">
                 Track package
               </button>
@@ -52,7 +54,6 @@ function loadPage(){
   
     const orderTimeString = dayJs(order.orderTime).format('MMMM D');
     const orderTotalCost = formatCurrency(order.totalCostCents);
-    const orderId = order.id; 
     
     ordersHTML += `
       <div class="order-container">
@@ -70,7 +71,7 @@ function loadPage(){
   
           <div class="order-header-right-section">
             <div class="order-header-label">Order ID:</div>
-            <div>${orderId}</div>
+            <div>${order.id}</div>
           </div>
         </div>
   
@@ -81,4 +82,20 @@ function loadPage(){
     `;
   });
   document.querySelector('.js-orders-grid').innerHTML = ordersHTML;
+
+
+  document.querySelectorAll('.js-buy-again-button')
+    .forEach((button) => {
+      button.addEventListener('click', () => {
+        const { productId } =button.dataset; 
+        cart.addToCart(productId, 1);
+        button.innerHTML = 'Added';
+      setTimeout(() => {
+        button.innerHTML = `
+          <img class="buy-again-icon" src="images/icons/buy-again.png">
+          <span class="buy-again-message">Buy it again</span>
+        `;
+      }, 1000);
+      })
+    })
 }
