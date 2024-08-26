3c4985a7e7ef8fbe21986c7be44fbc6ef14d86f3
Dhruv Sabharwal <dhruvsabharwalh7@gmail.com>
2024-08-27 02:13:45 +0530

    No products found message

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -18,67 +18,75 @@ function renderProductsGrid(){
     });
   }
   let productsHTML = '';
-  filteredProducts.forEach((product) => {
-    productsHTML += `
-      <div class="product-container">
-        <div class="product-image-container">
-          <img class="product-image"
-            src="${product.image}">
-        </div>
-
-        <div class="product-name limit-text-to-2-lines">
-          ${product.name}
-        </div>
-
-        <div class="product-rating-container">
-          <img class="product-rating-stars"
-            src="${product.getStarsUrl()}">
-          <div class="product-rating-count link-primary">
-            ${product.rating.count}
+  if (filteredProducts.length !== 0){
+    filteredProducts.forEach((product) => {
+      productsHTML += `
+        <div class="product-container">
+          <div class="product-image-container">
+            <img class="product-image"
+              src="${product.image}">
           </div>
-        </div>
 
-        <div class="product-price">
-          ${product.getPrice()}
-        </div>
+          <div class="product-name limit-text-to-2-lines">
+            ${product.name}
+          </div>
 
-        <div class="product-quantity-container">
-          <select class="js-quantity-selector-${product.id}">
-            <option selected value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-            <option value="6">6</option>
-            <option value="7">7</option>
-            <option value="8">8</option>
-            <option value="9">9</option>
-            <option value="10">10</option>
-          </select>
-        </div>
-        <!-- 
-          we can use ternary operator to check if the product is instanceof Clothing if yes then we can return the link to size chart else return an empty string 
-        -->
-        <!-- 
-          1)But we have used 'Polymorphism'. 
-          2)Polymorphism : use a method w/o knowing the class.
-          3)extraInfoHTML() is defined in both the parent and child class but it returns the link to size chart in child class (Clothing) and returns an empty string in parent class (Product). 
-        -->
-        ${product.extraInfoHTML()}
+          <div class="product-rating-container">
+            <img class="product-rating-stars"
+              src="${product.getStarsUrl()}">
+            <div class="product-rating-count link-primary">
+              ${product.rating.count}
+            </div>
+          </div>
 
-        <div class="product-spacer"></div>
+          <div class="product-price">
+            ${product.getPrice()}
+          </div>
 
-        <div class="added-to-cart js-added-to-cart-${product.id}">
-          <img src="images/icons/checkmark.png">
-          Added
-        </div>
+          <div class="product-quantity-container">
+            <select class="js-quantity-selector-${product.id}">
+              <option selected value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select>
+          </div>
+          <!-- 
+            we can use ternary operator to check if the product is instanceof Clothing if yes then we can return the link to size chart else return an empty string 
+          -->
+          <!-- 
+            1)But we have used 'Polymorphism'. 
+            2)Polymorphism : use a method w/o knowing the class.
+            3)extraInfoHTML() is defined in both the parent and child class but it returns the link to size chart in child class (Clothing) and returns an empty string in parent class (Product). 
+          -->
+          ${product.extraInfoHTML()}
 
-        <button class="add-to-cart-button button-primary js-add-to-cart-button"
-        data-product-id = "${product.id}">
-          Add to Cart
-        </button>
-      </div>`;
-  });
+          <div class="product-spacer"></div>
+
+          <div class="added-to-cart js-added-to-cart-${product.id}">
+            <img src="images/icons/checkmark.png">
+            Added
+          </div>
+
+          <button class="add-to-cart-button button-primary js-add-to-cart-button"
+          data-product-id = "${product.id}">
+            Add to Cart
+          </button>
+        </div>`;
+    });
+  }else{
+    productsHTML += `
+    <div class="empty-results-message">
+      No products matched your search.
+    </div>
+    `;
+  }
   document.querySelector('.js-products-grid')
     .innerHTML = productsHTML ;
 
